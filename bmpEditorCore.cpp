a852a1e49d180f7aae33d359d6a2c733177e1a42
verybadcoder01 <76253661+verybadcoder01@users.noreply.github.com>
2022-01-19 11:08:54 +0300

    fixed one coloured bmp

--- bmpEditorCore.cpp
+++ bmpEditorCore.cpp
@@ -4,11 +4,9 @@ std::mt19937 rnd(std::chrono::high_resolution_clock::now().time_since_epoch().co
 
 namespace BMP {
 
-    unsigned char transformToDecimal(const std::string &hex) {
+    unsigned char transformToDecimal(const char *hex) {
         unsigned char ans = 0;
-        for (int i = (int) hex.size() - 1; i >= 0; --i) {
-            ans += (hex[i] - '0') * (int) pow(16, (int) hex.size() - i);
-        }
+        sscanf(hex, "%02hhx", &ans);
         return ans;
     }
 
@@ -21,7 +19,7 @@ namespace BMP {
     class ImageBMP {
     private:
         std::string fileName;
-        std::pair<unsigned int, unsigned int> widhtAndHeight;
+        std::pair<unsigned int, unsigned int> widthAndHeight;
 
         struct Pixel {
             unsigned char red, green, blue;
@@ -42,16 +40,16 @@ namespace BMP {
             inputFile.seekg(18);
             unsigned int curr;
             inputFile.read((char *) &curr, 4);
-            widhtAndHeight.first = curr;
+            widthAndHeight.first = curr;
             inputFile.read((char *) &curr, 4);
-            widhtAndHeight.second = curr;
+            widthAndHeight.second = curr;
             inputFile.seekg(10);
             unsigned int PixelDataOffset;
             inputFile.read((char *) &PixelDataOffset, 4);
             inputFile.seekg(PixelDataOffset);
-            rawPixelData.resize(widhtAndHeight.second);
-            for (int i = 0; i < widhtAndHeight.second; ++i) {
-                for (int j = 0; j < widhtAndHeight.first; ++j) {
+            rawPixelData.resize(widthAndHeight.second);
+            for (int i = 0; i < widthAndHeight.second; ++i) {
+                for (int j = 0; j < widthAndHeight.first; ++j) {
                     rawPixelData[i].push_back({});
                     inputFile.read((char *) &curr, 1);
                     rawPixelData[i].back().red = curr;
@@ -60,7 +58,7 @@ namespace BMP {
                     inputFile.read((char *) &curr, 1);
                     rawPixelData[i].back().blue = curr;
                 }
-                for (int j = 0; j < (4 - widhtAndHeight.first * 3 % 4) % 4; ++j) {
+                for (int j = 0; j < (4 - widthAndHeight.first * 3 % 4) % 4; ++j) {
                     char byte;
                     inputFile.read(&byte, 1);
                 }
@@ -72,24 +70,24 @@ namespace BMP {
         }
 
         [[nodiscard]] unsigned int getHeight() const {
-            return widhtAndHeight.second;
+            return widthAndHeight.second;
         }
 
         [[nodiscard]] unsigned int getWidth() const {
-            return widhtAndHeight.first;
+            return widthAndHeight.first;
         }
 
         void changePixel(int i, int j, Pixel newPixel) {
-            if (i > rawPixelData.size() || rawPixelData.size() == 0 || rawPixelData[0].size() <= j) {
+            if (i > rawPixelData.size() || rawPixelData.empty() || rawPixelData[0].size() <= j) {
                 std::cerr << "YOU ARE DUMB\n";
-                throw SIGSEGV;
+                exit(SIGSEGV);
             }
             rawPixelData[i][j].red = newPixel.red;
             rawPixelData[i][j].blue = newPixel.blue;
             rawPixelData[i][j].green = newPixel.green;
         }
 
-        Pixel makeGrey(Pixel thisPixel) {
+        static Pixel makeGrey(Pixel thisPixel) {
             unsigned int R = thisPixel.red, B = thisPixel.blue, G = thisPixel.green;
             thisPixel.red = (R + B + G) / 3;
             thisPixel.blue = (R + B + G) / 3;
@@ -99,25 +97,25 @@ namespace BMP {
 
         void save() {
             std::ofstream outputFile(fileName, std::ios_base::binary);
-            createHeader(outputFile, widhtAndHeight.first, widhtAndHeight.second);
-            if (rawPixelData.size() < widhtAndHeight.second) {
-                rawPixelData.resize(widhtAndHeight.second);
+            createHeader(outputFile, widthAndHeight.first, widthAndHeight.second);
+            if (rawPixelData.size() < widthAndHeight.second) {
+                rawPixelData.resize(widthAndHeight.second);
             }
             outputFile.seekp(54);
-            for (int i = 0; i < widhtAndHeight.second; ++i) {
-                for (int j = 0; j < widhtAndHeight.first; ++j) {
+            for (int i = 0; i < widthAndHeight.second; ++i) {
+                for (int j = 0; j < widthAndHeight.first; ++j) {
                     outputFile.write((char *) &rawPixelData[i][j].red, 1);
                     outputFile.write((char *) &rawPixelData[i][j].green, 1);
                     outputFile.write((char *) &rawPixelData[i][j].blue, 1);
                 }
-                for (int j = 0; j < (4 - widhtAndHeight.first * 3 % 4) % 4; ++j) {
+                for (int j = 0; j < (4 - widthAndHeight.first * 3 % 4) % 4; ++j) {
                     char byte = 0;
                     outputFile.write(&byte, 1);
                 }
             }
         }
 
-        void createHeader(std::ostream &outputFile, unsigned int width, unsigned int height) {
+        static void createHeader(std::ostream &outputFile, unsigned int width, unsigned int height) {
             outputFile.write("BM", 2);
             unsigned int k = (width * 3) + ((4 - width * 3 % 4) % 4) * height + 54;
             outputFile.write((char *) &k, 4);
@@ -140,15 +138,15 @@ namespace BMP {
         void save(std::string FileName) {
             fileName = FileName;
             std::ofstream outputFile(FileName, std::ios_base::binary);
-            createHeader(outputFile, widhtAndHeight.first, widhtAndHeight.second);
+            createHeader(outputFile, widthAndHeight.first, widthAndHeight.second);
             outputFile.seekp(54);
-            for (int i = 0; i < widhtAndHeight.second; ++i) {
-                for (int j = 0; j < widhtAndHeight.first; ++j) {
+            for (int i = 0; i < widthAndHeight.second; ++i) {
+                for (int j = 0; j < widthAndHeight.first; ++j) {
                     outputFile.write((char *) &rawPixelData[i][j].red, 1);
                     outputFile.write((char *) &rawPixelData[i][j].green, 1);
                     outputFile.write((char *) &rawPixelData[i][j].blue, 1);
                 }
-                for (int j = 0; j < (4 - widhtAndHeight.first * 3 % 4) % 4; ++j) {
+                for (int j = 0; j < (4 - widthAndHeight.first * 3 % 4) % 4; ++j) {
                     char byte = 1;
                     outputFile.write(&byte, 1);
                 }
@@ -157,14 +155,20 @@ namespace BMP {
 
         ImageBMP(std::string &_fileName, unsigned int width, unsigned int height, std::string &backGroundColor) {
             fileName = _fileName;
-            widhtAndHeight.first = width;
-            widhtAndHeight.second = height;
+            widthAndHeight.first = width;
+            widthAndHeight.second = height;
             std::ofstream outputFile(_fileName, std::ios_base::binary);
             rawPixelData.resize(height);
             createHeader(outputFile, width, height);
-            unsigned char R = transformToDecimal(std::string({backGroundColor[0], backGroundColor[1]}));
-            unsigned char G = transformToDecimal(std::string({backGroundColor[2], backGroundColor[3]}));
-            unsigned char B = transformToDecimal(std::string({backGroundColor[4], backGroundColor[5]}));
+            std::string a = std::string({backGroundColor[0], backGroundColor[1]});
+            const char* cod = a.c_str();
+            unsigned char R = transformToDecimal(cod);
+            a = std::string({backGroundColor[2], backGroundColor[3]});
+            cod = a.c_str();
+            unsigned char G = transformToDecimal(cod);
+            a = std::string({backGroundColor[4], backGroundColor[5]});
+            cod = a.c_str();
+            unsigned char B = transformToDecimal(cod);
             for (int i = 0; i < height; ++i) {
                 for (int j = 0; j < width; ++j) {
                     rawPixelData[i].push_back({});
@@ -183,8 +187,8 @@ namespace BMP {
         }
 
         void disableChannel(RGB channel) {
-            for (int i = 0; i < widhtAndHeight.second; ++i) {
-                for (int j = 0; j < widhtAndHeight.first; ++j) {
+            for (int i = 0; i < widthAndHeight.second; ++i) {
+                for (int j = 0; j < widthAndHeight.first; ++j) {
                     switch (channel) {
                         case RGB::RED:
                             rawPixelData[i][j].blue = 0;
@@ -201,25 +205,25 @@ namespace BMP {
         }
 
         void toGreyScale() {
-            for (int i = 0; i < widhtAndHeight.second; ++i) {
-                for (int j = 0; j < widhtAndHeight.first; ++j) {
+            for (int i = 0; i < widthAndHeight.second; ++i) {
+                for (int j = 0; j < widthAndHeight.first; ++j) {
                     rawPixelData[i][j] = makeGrey(rawPixelData[i][j]);
                 }
             }
         }
 
         void flipHorizontal() {
-            for (int i = 0; i < widhtAndHeight.second; ++i) {
-                for (int j = 0; j < widhtAndHeight.first / 2; ++j) {
-                    std::swap(rawPixelData[i][j], rawPixelData[i][widhtAndHeight.first - j - 1]);
+            for (int i = 0; i < widthAndHeight.second; ++i) {
+                for (int j = 0; j < widthAndHeight.first / 2; ++j) {
+                    std::swap(rawPixelData[i][j], rawPixelData[i][widthAndHeight.first - j - 1]);
                 }
             }
         }
 
         void flipVertical() {
-            for (int i = 0; i < widhtAndHeight.second / 2; ++i) {
-                for (int j = 0; j < widhtAndHeight.first; ++j) {
-                    std::swap(rawPixelData[i][j], rawPixelData[widhtAndHeight.second - i - 1][j]);
+            for (int i = 0; i < widthAndHeight.second / 2; ++i) {
+                for (int j = 0; j < widthAndHeight.first; ++j) {
+                    std::swap(rawPixelData[i][j], rawPixelData[widthAndHeight.second - i - 1][j]);
                 }
             }
         }
@@ -234,7 +238,7 @@ namespace BMP {
                 }
             }
             rawPixelData = ans;
-            std::swap(widhtAndHeight.first, widhtAndHeight.second);
+            std::swap(widthAndHeight.first, widthAndHeight.second);
             save();
         }
         void turnRight(){
@@ -249,10 +253,9 @@ using namespace BMP;
 
 int main() {
     //ignore main
-    std::string s = "matinf_photo.bmp", s1 = "amoga.bmp", s2 = "1.bmp";
+    std::string s = "matinf_photo.bmp", s1 = "amoga.bmp", s2 = "1.bmp", c = "FFFFFF";
     ImageBMP amoga(s);
     amoga.save(s1);
-    amoga.turnLeft();
-    amoga.turnRight();
+    ImageBMP sus(s2, 40, 40, c);
     return 0;
 }
